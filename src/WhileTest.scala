fc1e8a3884dc43c0fcc030cd09a10a8de9d68c10
Vegetab1eDog <xuyang21@mails.jlu.edu.cn>
2025-04-15 10:17:29 +0800

    first commit

--- src/WhileTest.scala
+++ src/WhileTest.scala
@@ -1,10 +1,10 @@
 object WhileTest {
-  def main(args: Array[String]) {
-    var a = 10;
+  def main(args: Array[String]): Unit = {
+    var a = 10
     // 无限循环
     while( a<100 ){
       a=a+1
-      println( "a 的值为 : " + a );
+      println( "a 的值为 : " + a )
     }
   }
 }
